659afb31c0fbc596eee94acb5532ba17923cf34c
rost2001 <72069744+rost2001@users.noreply.github.com>
2021-03-30 05:58:53 +0100

    version 1 done

---
+++ cart.js
@@ -0,0 +1,56 @@
+const form = document.getElementById("form");
+const customerName = document.getElementById("name");
+const telephone = document.getElementById("telephone");
+const email = document.getElementById("email");
+const address = document.getElementById("address");
+const errorEl = document.getElementById("error");
+
+
+
+
+form.addEventListener("submit", (e) => {
+    //e.preventDefault()
+    let messages = [];
+
+    if (customerName.value.trim() === "" || customerName.value == null) {
+        messages.push("Du måste fylla i namn")
+    }
+
+    if (telephone.value.trim() === "" || telephone.value == null) {
+        messages.push("Du måste fylla i telefon nr")
+    }
+
+    if (email.value.trim() === "" || email.value == null) { // kolla ordentligt
+        messages.push("Du måste fylla i e-mail")
+    }
+
+    if (address.value.trim() === "" || address.value == null) {
+        messages.push("Du måste fylla i adress")
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault();
+        errorEl.innerText = messages.join(", ");
+    }
+    
+});
+
+const product = JSON.parse(localStorage.getItem("product"));
+
+
+let output =  `
+<div class="col-lg-4 col-md-6 mb-4">
+<div class="card h-100">
+  <a href="#"><img class="card-img-top img-thumbnail" src="${product.image}" alt=""></a>
+  <div class="card-body">
+    <h4 class="card-title">
+      <a href="#">${product.title}</a>
+    </h4>
+    <h5>${product.price}</h5>
+    <p class="card-text">${product.description}</p>
+  </div>
+</div>
+</div>
+`;
+  
+let div = document.getElementById("product").innerHTML = output;
--- fakestore.js
+++ fakestore.js
@@ -1,20 +1,31 @@
-let endpoint = "http://webacademy.se/fakestore/";
+//let endpoint = "http://webacademy.se/fakestore/";
+let endpoint = "https://fakestoreapi.com/products";
 //let endpoint = "fakestoreAPI.json";
 // måste ändra till webbplatsen sen
-// sista 02.10
 
- fetch(endpoint)
-.then((response) => response.json())
-.then((data) => render(data))
-.catch(error => console.error(error));
+window.addEventListener("load", load);
+
+
+function load() {
+    fetch(endpoint)
+    .then((response) => response.json())
+    .then((data) => render(data))
+    .catch(error => console.error(error));
+}
+
+
+setFromSession();
+
  
 function render(products) {
-    console.log(products);
+
+   window.sessionStorage.setItem("products", JSON.stringify(products));
+
     let output ="";
     products.forEach((product) => (output += `
     <div class="col-lg-4 col-md-6 mb-4">
     <div class="card h-100">
-      <a href="#"><img class="card-img-top" src="${product.image}" alt=""></a>
+      <a href="#"><img class="card-img-top img-thumbnail pic" src="${product.image}" alt=""></a>
       <div class="card-body">
         <h4 class="card-title">
           <a href="#">${product.title}</a>
@@ -23,10 +34,50 @@ function render(products) {
         <p class="card-text">${product.description}</p>
       </div>
       <div class="card-footer text-center">
-        <a href="#" class="btn btn-primary w-50">Lägg till</a> 
+        <a href="cart.html" class="btn btn-primary w-50" id="${product.id}">Lägg till</a> 
       </div>
     </div>
   </div>
     `));
     document.getElementById("products").innerHTML = output; 
+    buttons();
 }
+// lägg tillbaka href till cart sen #
+// klicka på linken så ska det sparas i localstorage
+
+
+function myFunction(){
+    let id = this.id;
+    console.log("okok");
+    //let id = document.getElementById(this); // this
+    console.log(id);
+}
+
+function buttons(){
+    let btns = document.getElementsByClassName("btn");
+    console.log(btns.length);
+   for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", setFromSession); // ska vara get from session
+   }
+}
+
+
+function setFromSession() {
+    let id = this.id;
+
+    const arr = JSON.parse(sessionStorage.getItem("products"));
+    //console.log(Array.isArray(arr));
+    //console.log(arr);
+
+    arr.forEach(o => {
+        //console.log(o); // set in localstorage to access from cart
+        if (o.id == id) {
+            console.log("same same" + o.title);
+            window.localStorage.setItem("product", JSON.stringify(o));
+        }
+    });
+
+}
+
+
+// on click button add to local storage
