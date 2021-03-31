d13d0a6f01750b97b7ada8231c6e8e111ab17f54
rost2001 <72069744+rost2001@users.noreply.github.com>
2021-03-31 12:02:58 +0100

    obect-fit: contain för att visa bilderna

--- fakestore.js
+++ fakestore.js
@@ -28,7 +28,7 @@ function render(products) {
       <a href="#"><img class="card-img-top img-thumbnail pic" src="${product.image}" alt=""></a>
       <div class="card-body">
         <h4 class="card-title">
-          <a href="#">${product.title}</a>
+          <a href="#" disabled="disabled">${product.title}</a>
         </h4>
         <h5>${product.price}</h5>
         <p class="card-text">${product.description}</p>
